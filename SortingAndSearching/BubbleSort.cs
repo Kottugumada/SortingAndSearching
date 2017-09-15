ac50538ca9ab1ea00760b002e5e31cd60601aa02
Karthik Kottugumada <karthikkottugumada@outlook.com>
2017-09-15 11:55:03 -0700

    Initial Commit

---
+++ SortingAndSearching/BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAndSearching
+{
+    class BubbleSort
+    {
+      public int[] sortArray(int[] arr)
+        {
+            int[] brr = arr;
+            int changes = 0;
+            for (int i = 0; i<arr.Length -1; i++)
+            {
+                if (arr[i] > arr[i+1])
+                {
+                    int temp;
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    changes = changes + 1;
+                }
+            }
+            if (changes == 0)
+            {
+                return arr;
+            }
+            else
+            {
+              return sortArray(arr);
+            }
+            
+        }
+        
+
+    }
+}
---
+++ SortingAndSearching/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SortingAndSearching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort bubbleSort = new BubbleSort();
+            int[] unsorted = new int[5] { 5, 1, 4, 2, 8 };
+            int[] sorted = bubbleSort.sortArray(unsorted);
+            for (int i = 0; i < unsorted.Length; i++)
+            {
+                Console.WriteLine(sorted[i] + ",");
+            }
+            Console.Read();
+        }
+    }
+}
